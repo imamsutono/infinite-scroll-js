0f8fef318f2029491eda242c7d6fa9f043621fdb
imamsutono <imamssutono@gmail.com>
2018-09-28 20:34:38 +0700

    Add current data for onScrollEnd callback

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -5,13 +5,13 @@ function infinite(options) {
   var displayed = options.dataShowOnStart;
 
   for (var i = 0; i < options.dataShowOnStart; i++) {
-    options.onScrollEnd(i);
+    options.onScrollEnd(data[i], i);
   }
 
   el.addEventListener('scroll', function() {
     if ((el.scrollTop + el.clientHeight >= el.scrollHeight) && displayed < data.length) {
       for (var a = displayed; a < displayed + dataShow - 1; a++) {
-        options.onScrollEnd(a);
+        options.onScrollEnd(data[a], a);
       }
 
       displayed = displayed + dataShow;
